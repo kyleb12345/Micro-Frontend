72a5b2b6691ce3fdfc4667ab63e9361eed87ad87
kyle <bartscholar@gmail.com>
2022-06-20 19:22:48 -0500

    helped files with async situation

--- products/src/index.js
+++ products/src/index.js
@@ -1,10 +1,3 @@
-import faker from 'faker';
+import('./bootstrap');
 
-let products = '';
-
-for (let i =0; i < 5; i++) {
-    const name =  faker.commerce.productName();
-    products += `<div>${name}</div>`;
-}
-
-document.querySelector('#dev-products').innerHTML = products;
+//helps make code run async
